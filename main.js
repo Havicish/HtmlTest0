b2c65b1b1146d643204a6466de39d07af6145f07
Havicish <157093976+Havicish@users.noreply.github.com>
2025-06-11 21:46:06 +0000

    e19

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
+import E from './test.js';
+
+E.test();
+
+console.log(E.thing);
+
 alert('Page has loaded');
---
+++ test.js
@@ -0,0 +1,7 @@
+export function test() {
+  console.log('Test function called');
+}
+
+export let thing = {
+    X: 1
+}
